aa6e64be10cc8f71e6a157f9bb38837939b3292e
Ali Abdul Basit <Aliabdulbasit2002@gmail.com>
2023-03-14 13:34:45 +0000

    My first react Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 style={{ color: "crimson" }}>
+        Codetrain is awesome, react workks!!!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
